cb55b12b06551d916d91563318e26286fe10e63e
leenah1997 <55778880+leenah1997@users.noreply.github.com>
2022-08-21 10:27:45 +0300

    first commit

---
+++ src/main/java/com/example/movieticketsystem/MovieticketsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.movieticketsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieticketsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieticketsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/movieticketsystem/MovieticketsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movieticketsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieticketsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
